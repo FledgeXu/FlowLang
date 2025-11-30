4a49823d190d88df83799488be19db000fd0f82b
Fledge Shiu <xzk0701@gmail.com>
2025-11-30 15:33:10 +0800

    Add exception_handler.

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,10 @@
 import spacy
-from fastapi import APIRouter, FastAPI
+from fastapi import APIRouter, FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.v1 import article, word
 from app.core.setting import SETTING
+from app.middleware.exception_handler import http_exception_handler
 
 LANGUAGE_MODEL = {
     "en": spacy.load("en_core_web_lg"),
@@ -12,6 +13,8 @@ LANGUAGE_MODEL = {
 }
 
 app = FastAPI()
+
+app.exception_handler(HTTPException)(http_exception_handler)
 
 app.add_middleware(
     CORSMiddleware,
---
+++ backend/app/middleware/exception_handler.py
@@ -0,0 +1,12 @@
+from fastapi import HTTPException, Request
+from fastapi.responses import JSONResponse
+
+
+async def http_exception_handler(_: Request, exc: HTTPException):
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={
+            "code": exc.status_code,
+            "message": exc.detail,
+        },
+    )
